1b0ec2abdcef307daf1a3acbf081990eef95283b
navinczernev <navinczernev@gmail.com>
2024-09-14 13:47:18 +0530

    Add dog image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Image, Label, Orientation};
 
 fn main() {
     let app = Application::new(
@@ -10,8 +10,20 @@ fn main() {
     app.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
         window.set_title("Dogsay");
-        window.set_default_size(350, 70);
+        window.set_default_size(500, 590);
 
+        let layout_box = GtkBox::new(Orientation::Vertical, 0);
+
+        let label = Label::new(
+            Some("Woof!\n     \\\n       \\")
+        );
+        layout_box.add(&label);
+        let dog_image = Image::from_file(
+            "./images/dog.png"
+        );
+        layout_box.add(&dog_image);
+
+        window.add(&layout_box);
         window.show_all();
     });
 
